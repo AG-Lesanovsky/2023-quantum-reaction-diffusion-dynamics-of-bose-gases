d4f677db4cd319cfe66d1234bdb246f97df411a8
ShiphrahR <shiphrah@gmx.de>
2024-03-27 09:33:50 +0100

    added coagulation + data

--- src/qrd_dynamics.py
+++ src/qrd_dynamics.py
@@ -11,14 +11,14 @@ import matplotlib.pyplot as plt
 L = 4000
 DISTANCE = 1
 N_BOSONS = 4000
-REL_TOL = 1e-8
-ABS_TOL = 1e-8
+REL_TOL = 1e-4
+ABS_TOL = 1e-4
 T_START = 0
-T_END = 1e6
+T_END = 1e4
 SCALE = 0.5
 
 # Plot values
-time_steps = np.logspace(-3,6, 200)
+time_steps = np.logspace(-3,4, 200)
 scaled_time = SCALE*time_steps
 
 superposition_angle = [np.pi/4, np.pi/3, np.pi/5]
@@ -98,38 +98,66 @@ def first_order_annihilation(t: np.array, n: np.array,
     _ = (t,)
     return result
 
-def second_order_annihilation(t: np.array, n: np.array, length: float):
+def second_order_annihilation(t: np.array, n: np.array, length: float) -> np.array:
+    """Defines right-hand-side of rate equation for second-order annihilation dynamics of bosons.
+    The paramemter n (boson density) depends on t.
 
+    Args:
+        t (np.array): time
+        n (np.array): density
+        length (float): lattice length
+
+    Returns:
+        np.array: returns right-hand-side of rate equation to be solved
+    """
     k = -np.pi + 2*np.pi/length*np.arange(1, length+1)
     term_1 = np.matmul(n, np.power(np.cos(k),2))
     term_3 = np.sum(n)
     term_2 = np.matmul(n, np.cos(k))
-    result = -4/length*n*(term_1 + 2*term_2*np.cos(k) + term_3*(np.power(np.cos(k),2) -4*np.cos(k)+ 4) - 4*term_2)
+    result = -4/length*n*(term_1 + 2*term_2*np.cos(k) + term_3*(np.power(np.cos(k),2)
+                            -4*np.cos(k)+ 4) - 4*term_2)
+    _ = (t,)
+    return result
+
+def coagulation(t: np.array, n: np.array) -> np.array:
+    """Defines right-hand-side of rate equation for coagulation dynamics of bosons.
+    The paramemter n (boson density) depends on t.
+
+    Args:
+        t (np.array): time
+        n (np.array): density
+
+    Returns:
+        np.array: returns right-hand-side of rate equation to be solved
+    """
+    result = -(6*np.power(n,3)  + 2*np.power(n,2))
     _ = (t,)
     return result
 
 
-# Choosing initial condition
-initial = n_init_gauss()
 
+# Choosing initial condition & rate equation
+initial = [n_init_gauss(), n_init_flat()]
+rate_equation = second_order_annihilation
 
 # Solving the rate equation and calculating the effective exponent at each time step
-for index, item in enumerate(superposition_angle):
-    sol = solve_ivp(fun=first_order_annihilation, t_span=(T_START, T_END), y0=initial,
-            args=(L,1,item),atol=ABS_TOL, rtol=REL_TOL, t_eval=time_steps)
+for index, item in enumerate(initial):
+    sol = solve_ivp(fun=rate_equation, t_span=(T_START, T_END), y0=item,
+            args=(L,),atol=ABS_TOL, rtol=REL_TOL, t_eval=time_steps)
     average_density = np.sum(sol.y, axis=0)/len(sol.y)
 
-    sol_scaled = solve_ivp(fun=first_order_annihilation, t_span=np.array((T_START, T_END))*SCALE,
-            y0=initial, args=(L,1,item), t_eval=scaled_time, atol=ABS_TOL, rtol=REL_TOL)
+    sol_scaled = solve_ivp(fun=rate_equation, t_span=np.array((T_START, T_END))*SCALE,
+            y0=item, args=(L,), t_eval=scaled_time, atol=ABS_TOL, rtol=REL_TOL)
     average_density_scaled = np.sum(sol_scaled.y, axis=0)/len(sol_scaled.y)
     exponent = -np.log(average_density_scaled/average_density)/np.log(SCALE)
 
-    plt.plot(sol.t, average_density, label=str(item))
+    plt.plot(sol.t,exponent, label=str(item))
 
 
 plt.grid()
 plt.xlabel('time t')
 plt.xscale('log')
 plt.yscale('log')
+plt.axhline(y=0.3)
 plt.legend()
 plt.show()
